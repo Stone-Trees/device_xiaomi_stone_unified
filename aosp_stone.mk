72d928b615a8c0b35b4336188a0346c289889823
Drpto <dr.juniornoob@gmail.com>
2024-01-28 14:19:05 +0530

    stone: Go with HorizonDroid Official
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- aosp_stone.mk
+++ aosp_stone.mk
@@ -9,26 +9,29 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
+# HorizonDroid prop
+CUSTOM_BUILD_TYPE := Official
+HORIZON_MAINTAINER := Dr.Opto
+
 # Device props
-TARGET_SUPPORTS_BLUR := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_EPPE := true
 TARGET_DEBLOAT := true
 
 # Charging Animation
-TARGET_USE_PIXEL_CHARGER := true
+USE_PIXEL_CHARGER := true
 
 # Use Prebuilt Kernel
 PREBUILT_KERNEL := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := aosp_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
