58de7c0a849631647bb6ad523c3b9cddb327ad2b
Drpto <dr.juniornoob@gmail.com>
2024-01-28 14:19:05 +0530

    stone: Go with SuperiorExtended Official
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_stone.mk
+    $(LOCAL_DIR)/superior_stone.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_stone-user \
-    lineage_stone-userdebug \
-    lineage_stone-eng
+    superior_stone-user \
+    superior_stone-userdebug \
+    superior_stone-eng
--- superior_stone.mk
+++ superior_stone.mk
@@ -9,26 +9,40 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
 
-# Device props
+# Superior Extended
+TARGET_INCLUDE_MATLOG := false
+USE_MOTO_CALCULATOR := false
 TARGET_SUPPORTS_BLUR := true
+USE_MOTO_CLOCK := false
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
+BUILD_WITH_GAPPS := true
+USE_QUICKPIC := false
+USE_DUCKDUCKGO := false
+TARGET_INCLUDE_BACKUP_TOOL := true
+
+# Maintainer
+SUPERIOR_MAINTAINER := Dr.Opto
+
+# Device props
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_DISABLE_EPPE := true
 TARGET_DEBLOAT := true
 
 # Charging Animation
-TARGET_USE_PIXEL_CHARGER := true
+USE_PIXEL_CHARGER := true
 
 # Use Prebuilt Kernel
 PREBUILT_KERNEL := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := suoerior_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
