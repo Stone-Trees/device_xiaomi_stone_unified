eeea0e7b79eeb2ad0dc427f1e82bb37c67c1ce14
Drpto <dr.juniornoob@gmail.com>
2024-02-04 15:09:02 +0530

    stone: Go with EvolutionX official
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,11 @@
+# Device Repos [1/2]
+git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi_stone_unified.git -b 14 vendor/xiaomi
+
+git clone --depth=1 https://github.com/crdroidandroid/android_kernel_xiaomi_sm6375.git -b 14.0 kernel/xiaomi/sm6375
+
+git clone --depth=1 https://github.com/Stone-Trees/device_xiaomi_stone-kernel.git -b 14-old device/xiaomi/stone-kernel
+
+git clone --depth=1 https://github.com/Stone-Trees/hardware_xiaomi.git hardware/xiaomi
+
+#viper4Fx Repo [2/2]
+git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
