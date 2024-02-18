baaf4dfee5a3f1738df52cc63f99b8cb717ec8e2
drpto <dr.juniornoob@gmail.com>
2024-02-18 12:01:32 +0530

    Add some EvolutionX stuff
    Signed-off-by: drpto <dr.juniornoob@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,9 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)-kernel/ramdisk-modules/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)-kernel/vendor-modules/,$(TARGET_COPY_OUT_VENDOR)/lib/modules)
 
+#Force prebuilt kernel
+INLINE_KERNEL_PREBUILT := true
+
 # Media
 TARGET_USES_ION := true
 TARGET_DISABLED_UBWC := true
