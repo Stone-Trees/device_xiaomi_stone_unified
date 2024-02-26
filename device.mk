47ba03f450521997bfd212c1f11cb1b07af92e41
Arijit78 <sahaarijit2007@gmail.com>
2024-02-26 06:06:10 +0000

    stone: inherit overlay-evolution

--- device.mk
+++ device.mk
@@ -388,7 +388,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-evolution
 
 # RRO Overlays
 PRODUCT_PACKAGES += \
